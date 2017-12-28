075b3877280823b6478c2541c176bd3f93ac545f
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-28 15:39:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size > 1
     last = "and #{array.last}"
     array.pop
-    new_array = array.join(", ") << last
+    array.join(", ") << last
   else
     array[0]
   end
